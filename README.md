330e2688eab1267052540245daa4eb47db89f8d6
Tatsiujin Chin <c910335@gmail.com>
2019-08-14 19:17:41 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,39 @@
+# crie
+
+TODO: Write a description here
+
+## Installation
+
+1. Add the dependency to your `shard.yml`:
+
+   ```yaml
+   dependencies:
+     crie:
+       github: your-github-user/crie
+   ```
+
+2. Run `shards install`
+
+## Usage
+
+```crystal
+require "crie"
+```
+
+TODO: Write usage instructions here
+
+## Development
+
+TODO: Write development instructions here
+
+## Contributing
+
+1. Fork it (<https://github.com/your-github-user/crie/fork>)
+2. Create your feature branch (`git checkout -b my-new-feature`)
+3. Commit your changes (`git commit -am 'Add some feature'`)
+4. Push to the branch (`git push origin my-new-feature`)
+5. Create a new Pull Request
+
+## Contributors
+
+- [Tatsiujin Chin](https://github.com/your-github-user) - creator and maintainer
---
+++ src/crie.cr
@@ -0,0 +1,74 @@
+class Crie
+  VERSION = "0.1.0"
+  CT_ROOT = { "_keep" => nil }
+
+  macro add(pattern)
+    {% node = CT_ROOT %}
+    {% for char in pattern.chars %}
+      {% if !node[char] %}
+        {% node[char] = { "_keep" => nil} %}
+      {% end %}
+      {% node = node[char] %}
+    {% end %}
+  end
+
+  macro add_all(patterns)
+    {% for pattern in patterns %}
+      Crie.add({{pattern}})
+    {% end %}
+  end
+
+  macro add_suffixes_of(pattern)
+    {% c = 0 %}
+    {% for char in pattern.chars %}
+      Crie.add({{pattern[c..-1]}})
+      {% c = c + 1 %}
+    {% end %}
+  end
+
+  macro search(pattern)
+    {% node = CT_ROOT %}
+    {% c = 0 %}
+    {% for char in pattern.chars %}
+      {% if node[char] %}
+        {% c = c + 1%}
+        {% node = node[char] %}
+      {% else %}
+        {% node = { "_keep" => nil } %}
+      {% end %}
+    {% end %}
+    {{c}}
+  end
+
+  macro __to_named_tuple(node)
+    {% if node.size == 1%}
+      NamedTuple.new
+    {% else %}
+      {
+        {% for key in node.keys %}
+          {% if key != "_keep" %}
+            "{{key.id}}": __to_named_tuple({{node[key]}}),
+          {% end %}
+        {% end %}
+      }
+    {% end %}
+  end
+
+  macro finished
+    ROOT = __to_named_tuple({{CT_ROOT}})
+  end
+
+  def search(pattern)
+    node = ROOT
+    c = 0
+    pattern.each_char do |char|
+      if node[char.to_s]?
+        c += 1
+        node = node[char.to_s]
+      else
+        break
+      end
+    end
+    c
+  end
+end
